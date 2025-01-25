3a404015d1150c4e1055919187eef3471ed3ba21
SOMNATH <somnathnikam2076@gmail.com>
2025-01-25 12:26:40 +0530

    Second Commit

--- app/src/main/java/com/example/foodorder/SplashActivity.kt
+++ app/src/main/java/com/example/foodorder/SplashActivity.kt
@@ -1,11 +1,21 @@
 package com.example.foodorder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        Handler().postDelayed({
+            val intent = Intent(this, StartActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
+
+
     }
 }
